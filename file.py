214be4ada7856376dee2ffcd57f93ccd7a4798b7
Seif Khaled <seifkhaled2424@gmail.com>
2022-07-21 20:38:49 +0200

    interface basics

--- file.py
+++ file.py
@@ -1,10 +1,11 @@
+from asyncio import streams
+from curses import window
 from pytube import YouTube
-
-
+import tkinter as tk
 SAVE_PATH="D:/donwloaded_videos"
 
 
-youtube_url="https://www.youtube.com/watch?v=JGulAZnnTKA"
+youtube_url="https://www.youtube.com/watch?v=yoj2I6ZJLx8"
 
 yt=YouTube(youtube_url)
 
@@ -18,14 +19,34 @@ class download_vidoes:
     def get_title(self):
         return self.yt.title
     def donwload_video(self):
-       x= self.yt.streams.get_highest_resolution().download(self.savepath,filename=input("Enter file name (without extension): "))
-       print(x)
-    #    y=x.filter(res=self.reso,progressive=True,file_extension="mp4").first().download(self.savepath)
-    #    print(y)
+       x= self.yt.streams
+    #    print(x)
+       
+       y=x.filter(res=self.reso,progressive=True,file_extension="mp4")
+    #    .download(self.savepath,filename=input("Enter file name (without extension): "))
+       if(len(y)>0):
+            y=x.filter(res=self.reso,progressive=True,file_extension="mp4").first().download(self.savepath)
+       else:
+            return "couldnt find supported extension/file",x.filter(res=self.reso,progressive=True,file_extension="mp4")
+
+
+
+class interface:
+    def __init__(self,wid,hei):
+        self.width=wid
+        self.height=hei
+        self.window=tk.Tk()
+        self.window.geometry(str(wid)+"x"+str(hei))
+        self.window.title("Video Downloader Pro Max")
+        self.label = tk.Label(text="File path")
+        self.entry = tk.Entry()
+        self.window.mainloop()
     #    for i in x:
     #         for j in str(i.split()):
     #             print(j)
     
-
-c=download_vidoes(youtube_url,SAVE_PATH,"1080p")
-print(c.donwload_video())
+# print(help(filter))
+c=download_vidoes(youtube_url,SAVE_PATH,"720p")
+# print(c.donwload_video())
+# print(tk.TkVersion)
+i=
