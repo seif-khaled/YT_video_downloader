ed20b4641af718ec5187bcf76b786a6ee7a2f473
Seif Khaled <seifkhaled2424@gmail.com>
2022-07-24 14:43:09 +0200

    added another change in the final touch

--- file.py
+++ file.py
@@ -8,33 +8,6 @@ import sys
 import os.path
 from os import path
 import tkinter.font as tkFont
-# SAVE_PATH="D:/donwloaded_videos"
-
-
-# youtube_url="https://www.youtube.com/watch?v=yoj2I6ZJLx8"
-
-# yt=YouTube(youtube_url)
-
-# class download_vidoes:
-#     def __init__(self,youtubelink,savepath,resolution):
-#         self.youtube_url= youtubelink
-#         self.savepath=savepath
-#         self.reso=resolution
-#         self.yt=YouTube(youtube_url)
-#     # def get_reso(self):
-
-#     def get_title(self):
-#         return self.yt.title
-#     def donwload_video(self):
-#        x= self.yt.streams
-#     #    print(x)
-       
-#        y=x.filter(res=self.reso,progressive=True,file_extension="mp4")
-#     #    .download(self.savepath,filename=input("Enter file name (without extension): "))
-#        if(len(y)>0):
-#             y=x.filter(res=self.reso,progressive=True,file_extension="mp4").first().download(self.savepath)
-#        else:
-#             return "couldnt find supported extension/file",x.filter(res=self.reso,progressive=True,file_extension="mp4")
 
 
 
